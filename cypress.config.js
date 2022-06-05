e6c82247fc89b564face66734281e871a23b9b0b
shikdershondhi <ehatesham.ul-islam@welldev.io>
2022-06-05 12:02:12 +0600

    added plugins, cypress 10.0.2

---
+++ cypress.config.js
@@ -0,0 +1,21 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  watchForFileChanges: false,
+  video: true,
+  defaultCommandTimeout: 20000,
+  pageLoadTimeout: 60000,
+  viewportWidth:1360,
+  viewportHeight: 763,
+  retries: {
+    runMode: 1,
+    openMode: 1,
+  },
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config)
+    },
+  },
+})
